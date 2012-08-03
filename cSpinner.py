66560a14d8248818786b2b9b383ea025970b70cc
kxtells <jordi.kstells@gmail.com>
2012-08-03 12:32:06 +0200

    cSpinner has additional text option to give feedback on what country is being checked

--- cSpinner.py
+++ cSpinner.py
@@ -9,15 +9,19 @@ class cSpinner(threading.Thread):
     chars = ["\\","|","/","-"]
     index = 0
     keeprunning = True
-    paused = False; 
+    paused = False;
+    msg = ""
 
 
     def run(self):
         while self.keeprunning:
-            if (not self.paused): self.printing(self.chars[self.index%len(self.chars)])
+            if (not self.paused): self.printing(self.chars[self.index%len(self.chars)]+" "+self.msg)
             time.sleep(0.1)
             self.index +=1
 
+    def set_msg(self,text):
+        self.msg = text;
+    
     def printing(self,data):
         sys.stdout.write("\r\x1b[K"+data.__str__())
         sys.stdout.flush()
