a2c6ac3f86038393f7738b8ce4f59c0912de34b8
kxtells <jordi.kstells@gmail.com>
2012-08-07 18:47:52 +0200

    commented cPlace cReport and cSpinner

--- cPlace.py
+++ cPlace.py
@@ -1,8 +1,18 @@
 class cPlace():
     """
         Class containing information of a place
+        \details Entity simply used as a container of information.
     """
+    
     def __init__(self,name,lat,lon,abstract,country):
+        """
+            Class constructor
+            \param name
+            \param lat
+            \param lon
+            \param abstract
+            \param country
+        """
         self.name = name
         self.lat = lat
         self.lon = lon
--- cSpinner.py
+++ cSpinner.py
@@ -4,7 +4,8 @@ import time
 
 class cSpinner(threading.Thread):
     """
-        Print things to one line dynamically
+        Print information in the same line, giving feedback to the user
+        \details Prints a spinning text on the screen. Additional text may be attached as extra information. Extends the Thread class
     """
     chars = ["\\","|","/","-"]
     index = 0
@@ -14,23 +15,42 @@ class cSpinner(threading.Thread):
 
 
     def run(self):
+        """
+            \brief Start the thread
+        """
         while self.keeprunning:
-            if (not self.paused): self.printing(self.chars[self.index%len(self.chars)]+" "+self.msg)
+            if (not self.paused): self.__printing(self.chars[self.index%len(self.chars)]+" "+self.msg)
             time.sleep(0.1)
             self.index +=1
 
     def set_msg(self,text):
+        """
+            \brief Set the extra message to print
+            \param text String to print
+        """
         self.msg = text;
     
-    def printing(self,data):
+    def __printing(self,data):
+        """
+            \brief print the information to stdout
+        """
         sys.stdout.write("\r\x1b[K"+data.__str__())
         sys.stdout.flush()
 
     def stop(self):
+        """
+            \brief Stop the print thread.
+        """
         self.keeprunning = False
 
     def pause(self):
+        """
+            \brief Pause the print thread.
+        """
         self.paused = True;
 
     def unpause(self):
+        """
+            \brief continue the print thread.
+        """
         self.paused = False;
